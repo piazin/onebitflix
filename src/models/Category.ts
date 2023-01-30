f1d5d192de3a946cbbed8eed67f5d0e5569d384a
piazin <ls4803326@gmail.com>
2023-01-30 07:08:16 -0300

    feat(database/seeders): add seed in categories

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,30 +1,44 @@
-
-import { sequelize } from '../database'
-import { DataTypes, Model, Optional } from 'sequelize'
+import { sequelize } from '../database';
+import { DataTypes, Model, Optional } from 'sequelize';
 
 export interface Category {
-  id: number
-  name: string
-  position: number
+  id: number;
+  name: string;
+  position: number;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export interface CategoryCreationAttributes extends Optional<Category, 'id'> {}
 
-export interface CategoryInstance extends Model<Category, CategoryCreationAttributes>, Category {}
+export interface CategoryInstance
+  extends Model<Category, CategoryCreationAttributes>,
+    Category {}
 
-export const Category = sequelize.define<CategoryInstance, Category>('Category', {
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: DataTypes.INTEGER
-  },
-  name: {
-    allowNull: false,
-    type: DataTypes.STRING
-  },
-  position: {
-    allowNull: false,
-    type: DataTypes.INTEGER
+export const Category = sequelize.define<CategoryInstance, Category>(
+  'Category',
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    position: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    created_at: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updated_at: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
   }
-})
+);
